45a7b07b96cb1ddc85bea7f98658909dac77c2ce
DanielOttoni2 <0001251136@senaimgaluno.com.br>
2026-06-17 14:27:48 -0300

    Add files via upload

---
+++ desafio05.js
@@ -0,0 +1,7 @@
+let usuario = prompt("Digite o nome de usuário:");
+let senha = prompt("Digite a senha:");
+if (usuario === "admin" && senha === "senai123") {
+alert("Acesso concedido. Bem-vindo, Administrador!");
+} else { alert("Acesso negado. Usuário ou senha incorretos.");
+    
+}
